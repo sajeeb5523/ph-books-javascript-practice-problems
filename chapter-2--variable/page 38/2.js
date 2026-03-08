97e727ccf8d8a2cd3022f43be6b8e530c1a7f726
sajeeb5523 <sajeebaljabed1@gmail.com>
2026-03-09 01:02:47 +0600

    feat: complete code practice problems from page 38

---
+++ chapter-2--variable/page 38/2.js
@@ -0,0 +1,6 @@
+// তুই এই বছর যে কয়টা বই পড়ার পরিকল্পনা করছস, সেটাকে booksToRead নামে একটি ভেরিয়েবলে রাখ। পরে booksToRead-এর মান কনসোলে আউটপুট দেখা।
+
+
+
+const booksToRead = 12;
+console.log(booksToRead);
