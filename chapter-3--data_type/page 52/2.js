9b98caf76453d3ee34380b32eb7f16a117f3085c
sajeeb5523 <sajeebaljabed1@gmail.com>
2026-03-11 08:01:42 +0600

    feat: complete code practice problem solved from page 52

---
+++ chapter-3--data_type/page 52/2.js
@@ -0,0 +1,8 @@
+// তোর পকেটে এখন 500 টাকা আছে, pocketMoney নামে একটি ভেরিয়েবলে রাখ। পরে pocketMoney-এর মান 150 সেট কর এবং কনসোলে প্রিন্ট কর।
+
+
+
+let pocketMoney = 500;
+pocketMoney = 150;
+
+console.log(pocketMoney);
