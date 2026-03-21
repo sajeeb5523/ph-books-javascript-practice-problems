9c3fbb168bcefcf1f52f99e584d3fdaa503a7d2f
sajeeb5523 <sajeebaljabed1@gmail.com>
2026-03-21 17:29:39 +0600

    feat: complete code practice problem solved from page 109

---
+++ chapter-7--loop/page 109/1.js
@@ -0,0 +1,10 @@
+// while লুপ দিয়ে 10 বার কনসোল লগ কর। আমি প্রতিদিন মিনিমাম 3 ঘণ্টা করে প্রোগ্রামিং প্র্যাকটিস করব। 
+
+
+
+let num = 0;
+
+while (num <= 10) {
+    console.log(num + ": ami protidin minimum 3 ghonta kore programming practice korbo");
+    num++;
+}
