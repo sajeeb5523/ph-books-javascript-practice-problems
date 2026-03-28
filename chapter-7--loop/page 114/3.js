2550a01b82030a9a5e931e83119b54c410b043a9
sajeeb5523 <sajeebaljabed1@gmail.com>
2026-03-28 15:30:51 +0600

    feat: complete code practice problem solved from page 114

---
+++ chapter-7--loop/page 114/3.js
@@ -0,0 +1,10 @@
+// For লুপ দিয়ে 25 থেকে 75 পর্যন্ত সংখ্যাগুলোর যোগফল বের কর এবং যোগফল শেষে প্রিন্ট কর।
+
+
+
+let sum = 0;
+
+for (let num = 25; num <= 75; num++) {
+    sum = sum + num;
+}
+console.log("Sum of numbers: ", sum);
